398016a086643d30dd728a1842a7bce122772b63
myml <wurongjie1@gmail.com>
2018-04-09 13:09:03 +0800

    fix: paging unknown error

--- web-components/app-paging/app-paging.component.ts
+++ web-components/app-paging/app-paging.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import * as _ from 'lodash';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/observable/concat';
 
 @Component({
   selector: 'app-paging',
@@ -13,23 +14,26 @@ export class AppPagingComponent implements OnInit {
   @Input()
   set count(count: number) {
     this._count = count;
+    this.pageListObs = this.getPageList();
   }
   get count(): number {
     return this._count;
   }
   _count: number;
-  page: number;
-  pages: number[];
 
+  page = 1;
   pageSize = pagingSize;
   pageListObs: Observable<number[]>;
 
   constructor(private router: Router, private route: ActivatedRoute) {}
 
-  ngOnInit() {
-    this.pageListObs = this.route.params
-      .map(params => +params['page'])
+  ngOnInit() {}
+
+  getPageList(): Observable<number[]> {
+    return this.route.paramMap
+      .map(params => +params.get('page'))
       .map(page => {
+        console.log('test');
         this.page = page;
         return _.chain(1)
           .range(this.count + 1)
@@ -38,30 +42,7 @@ export class AppPagingComponent implements OnInit {
           .value();
       })
       .shareReplay();
-    // Observable.combineLatest(
-    //   this.route.paramMap,
-    //   this.route.queryParamMap
-    // ).subscribe(() => this._count > 1 && this.getPages.bind(this));
   }
-
-  // getPages() {
-  //   console.log(this._count);
-  //   const page = +this.route.snapshot.params['page'];
-  //   let pages = _.chain(1)
-  //     .range(this.count + 1)
-  //     .chunk(pagingSize)
-  //     .filter((chunk: number[]) => chunk.includes(page))
-  //     .last()
-  //     .value();
-  //   if (pages.length < pagingSize && pages.includes(this.count)) {
-  //     pages = _.chain(this.count - pagingSize + 1)
-  //       .range(this.count + 1)
-  //       .filter(n => n > 0)
-  //       .value();
-  //   }
-  //   this.page = page;
-  //   this.pages = pages;
-  // }
 
   goto(page: number) {
     this.router.navigate(['..', page], {
