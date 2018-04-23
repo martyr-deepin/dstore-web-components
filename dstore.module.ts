51928827d13891621eb6a414396eb19bfb892ea0
myml <wurongjie1@gmail.com>
2018-04-23 16:49:10 +0800

    baseServer do not is server

--- dstore.module.ts
+++ dstore.module.ts
@@ -9,7 +9,6 @@ import { AppService } from './services/app.service';
 import { CategoryService } from './services/category.service';
 import { DownloadingService } from './services/downloading.service';
 import { MaterializeService } from './services/materialize.service';
-import { BaseService } from './services/base.service';
 
 import { TitleComponent } from './components/title/title.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
@@ -47,7 +46,6 @@ import { SizeHuman } from './pipes/size-human';
     ReactiveFormsModule,
   ],
   providers: [
-    BaseService,
     AppService,
     CategoryService,
     DownloadingService,
