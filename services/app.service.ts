6455bc7c7c7b9b0f577af19464e5b1db8f05bd8b
myml <wurongjie1@gmail.com>
2018-05-11 16:12:16 +0800

    feat: app service

--- services/app.service.ts
+++ services/app.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, Subject, forkJoin, empty } from 'rxjs';
 import { retry, shareReplay, map, tap, flatMap, scan } from 'rxjs/operators';
 
-import * as _ from 'lodash';
+import { throttle, filter, compact, chain, get, cloneDeep } from 'lodash';
 
 import { BaseService } from './base.service';
 import { CategoryService } from './category.service';
@@ -23,9 +23,9 @@ export class AppService {
   private appsMap = new Map<string, App>();
   private lastModified: string;
   // 一秒的缓冲节流的_getAppMap
-  _getAppMapCache = _.throttle(this._getAppMap, 1000);
+  private _getAppMapCache = throttle(this._getAppMap, 1000);
   // 获取应用列表，应用名为键
-  _getAppMap(): Observable<Map<string, App>> {
+  private _getAppMap(): Observable<Map<string, App>> {
     return forkJoin(this.getAppListResult(), this.categoryServer.getList()).pipe(
       map(([result, categories]) => {
         if (!result.apps) {
@@ -33,10 +33,10 @@ export class AppService {
         }
         result.apps.forEach(app => {
           // set localInfo
-          if (_.get(app.locale, `${Locale.getUnixLocale()}.description.name`)) {
+          if (get(app.locale, [Locale.getUnixLocale(), 'description', 'name'])) {
             app.localInfo = app.locale[Locale.getUnixLocale()];
           } else {
-            app.localInfo = _.chain(app.locale)
+            app.localInfo = chain(app.locale)
               .toArray()
               .find(local => local.description.name !== '')
               .value();
@@ -51,29 +51,6 @@ export class AppService {
       }),
       shareReplay(),
     );
-  }
-
-  // 获取全部应用列表
-  getAppList(): Observable<App[]> {
-    return this._getAppMapCache().map(m => Array.from(m.values()));
-  }
-
-  // 根据应用名列表获取应用列表
-  getAppListByNames(appNames: string[]): Observable<App[]> {
-    return this._getAppMapCache().map(m => appNames.map(m.get));
-  }
-
-  // 根据应用名获取应用
-  getAppByName(name: string): Observable<App> {
-    return this._getAppMapCache().pipe(
-      map(m => _.cloneDeep(m.get(name))),
-      tap(app => console.log(`getAppByName:(${name}):`, app)),
-    );
-  }
-
-  // 设置Api网址
-  setApiURL(apiURL: string) {
-    this.apiURL = apiURL;
   }
 
   private getAppListResult(): Observable<Result> {
@@ -98,6 +75,44 @@ export class AppService {
       }),
       retry(3),
     );
+  }
+
+  // 获取全部应用列表
+  getAppList(): Observable<App[]> {
+    return this._getAppMapCache().pipe(map(m => compact(Array.from(m.values()))));
+  }
+
+  // 获取全部应用，以应用名为Key
+  getAppMap(): Observable<Map<string, App>> {
+    return this._getAppMapCache();
+  }
+
+  // 根据应用名获取应用
+  getAppByName(name: string): Observable<App> {
+    return this._getAppMapCache().pipe(
+      map(m => m.get(name)),
+      tap(app => console.log(`getAppByName:(${name}):`, app)),
+    );
+  }
+  // 根据应用名列表获取应用列表
+  getAppListByNames(appNames: string[]): Observable<App[]> {
+    return this._getAppMapCache().pipe(map(m => appNames.map(m.get.bind(m))));
+  }
+  getAppMapByNames(appNames: string[]): Observable<Map<string, App>> {
+    return this._getAppMapCache().pipe(
+      map(m => {
+        const appMap = new Map<string, App>();
+        appNames
+          .filter(appName => m.has(appName))
+          .map(appName => appMap.set(appName, m.get(appName)));
+        return appMap;
+      }),
+    );
+  }
+
+  // 设置Api网址
+  setApiURL(apiURL: string) {
+    this.apiURL = apiURL;
   }
 }
 
