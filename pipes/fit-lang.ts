151a660d40f818ad782c8b0a69e209cc3ad1449e
myml <wurongjie1@gmail.com>
2018-05-31 14:13:52 +0800

    fix: empty cursor and  fitLang pipe

--- pipes/fit-lang.ts
+++ pipes/fit-lang.ts
@@ -2,10 +2,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { environment } from '../../../environments/environment';
 // 在数组里选择合适的值，用于选择多语言和高清图片
 @Pipe({
-  name: 'fitLanguage'
+  name: 'fitLanguage',
 })
 export class FitLanguage implements PipeTransform {
   transform(value: string[]): string {
-    return value[0] || '';
+    if (navigator.language === 'zh-CN') {
+      return value[0] || value[1] || '';
+    } else {
+      return value[1] || value[0] || '';
+    }
   }
 }
