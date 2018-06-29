67b37b4ca8dc71684d6f0d4bdf15ff5c6cd836c4
myml <wurongjie1@gmail.com>
2018-06-29 14:43:48 +0800

    use localforage cache

--- services/app.service.ts
+++ services/app.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
-import { Observable, Subject, forkJoin, empty } from 'rxjs';
-import { retry, shareReplay, map, tap, flatMap, scan } from 'rxjs/operators';
+import { Observable, Subject, forkJoin, from, of } from 'rxjs';
+import { retry, shareReplay, map, tap, flatMap, scan, switchMap, last } from 'rxjs/operators';
 
 import { throttle, filter, compact, chain, get, cloneDeep, defaultsDeep } from 'lodash';
+
+import * as localForage from 'localforage';
 
 import { BaseService } from './base.service';
 import { CategoryService } from './category.service';
@@ -17,113 +19,148 @@ import { Locale } from '../utils/locale';
 export class AppService {
   private metadataService = BaseService.serverHosts.metadataServer;
   private apiURL = `${this.metadataService}/api/app`;
+  private store = localForage.createInstance({ name: 'apps' });
 
   constructor(private http: HttpClient, private categoryServer: CategoryService) {}
 
-  private appsMap = new Map<string, App>();
-  private lastModified: string;
-  // 一秒的缓冲节流的_getAppMap
-  private _getAppMapCache = throttle(this._getAppMap, 1000);
-  // 获取应用列表，应用名为键
-  private _getAppMap(): Observable<Map<string, App>> {
-    return forkJoin(this.getAppListResult(), this.categoryServer.getList()).pipe(
-      map(([result, categories]) => {
-        if (!result.apps) {
-          result.apps = [];
-        }
-        result.apps.forEach(app => {
-          // set localInfo
-          if (get(app.locale, [Locale.getUnixLocale(), 'description', 'name'])) {
-            app.localInfo = app.locale[Locale.getUnixLocale()];
-          } else {
-            app.localInfo = chain(app.locale)
-              .toArray()
-              .find(local => local.description.name !== '')
-              .value();
-          }
-          app.localInfo = defaultsDeep(app.localInfo, ...Object.values(app.locale), new App());
-          // set localCategory
-          app.localCategory = categories[app.category].LocalName || app.category;
-          // 增量覆盖
-          this.appsMap.set(app.name, app);
-        });
-        this.lastModified = result.lastModified;
-        localStorage.setItem('lastModified', this.lastModified);
-        localStorage.setItem('apps_raw', JSON.stringify(Array.from(this.appsMap.entries())));
-        return this.appsMap;
-      }),
-      shareReplay(),
-    );
+  get token() {
+    return localStorage.getItem('auth-token') || sessionStorage.getItem('auth-token');
+  }
+  get appsStoreKey() {
+    return this.token + '-apps';
+  }
+  get timeStoreKey() {
+    return this.token + '-time';
   }
 
-  private getAppListResult(): Observable<Result> {
-    return new Observable<Result>(obs =>
-      this.http
-        .get(this.apiURL, {
-          responseType: 'text',
-          params: this.lastModified ? { since: this.lastModified } : null,
-        })
-        .subscribe(
-          body => obs.next(JSON.parse(body, appReviver) as Result),
-          obs.error.bind(obs),
-          obs.complete.bind(obs),
-        ),
+  // 一秒的缓冲节流的_getAppMap
+  private _getAppMapCache = throttle(this._getAppMap, 1000);
+
+  // 获取应用列表，应用名为键
+  private _getAppMap() {
+    console.log(this.appsStoreKey, this.timeStoreKey);
+    return forkJoin<AppMap, string>(
+      this.store.getItem(this.appsStoreKey),
+      this.store.getItem(this.timeStoreKey),
     ).pipe(
-      tap(result => {
-        if (result.error && result.error.code === ErrorCode.CodeForceSync) {
-          this.lastModified = null;
-          this.appsMap.clear();
-          throw result.error.code;
+      switchMap(([appMap, lastTime]) => {
+        if (!appMap) {
+          appMap = {};
         }
-        if (result.deleted) {
-          result.deleted.map(appName => {
-            this.appsMap.delete(appName);
+        let params = new HttpParams();
+        if (lastTime) {
+          params = params.append('since', lastTime);
+        }
+        return forkJoin(
+          of(appMap),
+          of(lastTime),
+          this.http
+            .get(this.apiURL, { params, responseType: 'text' })
+            .pipe(map(body => JSON.parse(body, appReviver) as Result)),
+        );
+      }),
+      map(([appMap, lastTime, appMapChange]) => {
+        if (appMapChange.error) {
+          throw appMapChange.error;
+        }
+        if (lastTime === appMapChange.lastModified || !appMapChange.lastModified) {
+          return appMap;
+        }
+        lastTime = appMapChange.lastModified;
+        if (appMapChange.apps) {
+          appMapChange.apps.forEach(app => {
+            appMap[app.name] = app;
           });
         }
+        if (appMapChange.deleted) {
+          appMapChange.deleted.forEach(appName => {
+            delete appMap[appName];
+          });
+        }
+        this.store.clear().then(() => {
+          this.store.setItem(this.timeStoreKey, lastTime);
+          this.store.setItem(this.appsStoreKey, appMap);
+        });
+        return appMap;
       }),
-      retry(3),
+      flatMap(
+        appMap => {
+          return this.categoryServer.getList();
+        },
+        (appMap, categories) => {
+          Object.values(appMap).forEach(app => {
+            // set localInfo
+            app.localInfo =
+              app.locale[Locale.getUnixLocale()] ||
+              Object.values(app.locale).find(local => local.description.name !== '') ||
+              Object.values(app.locale)[0];
+            app.localInfo = defaultsDeep(app.localInfo, ...Object.values(app.locale), new App());
+            app.localCategory = categories[app.category].LocalName || app.category;
+          });
+          return appMap;
+        },
+      ),
     );
+
+    // return forkJoin(this.getAppListResult(), this.categoryServer.getList()).pipe(
+    //   map(([result, categories]) => {
+    //     if (!result.apps) {
+    //       result.apps = [];
+    //     }
+    //     result.apps.forEach(app => {
+    //       // set localInfo
+    //       if (get(app.locale, [Locale.getUnixLocale(), 'description', 'name'])) {
+    //         app.localInfo = app.locale[Locale.getUnixLocale()];
+    //       } else {
+    //         app.localInfo = chain(app.locale)
+    //           .toArray()
+    //           .find(local => local.description.name !== '')
+    //           .value();
+    //       }
+    //       app.localInfo = defaultsDeep(app.localInfo, ...Object.values(app.locale), new App());
+    //       // set localCategory
+    //       app.localCategory = categories[app.category].LocalName || app.category;
+    //       // 增量覆盖
+    //       // this.appsMap.set(app.name, app);
+    //       this.appsMap[app.name] = app;
+    //     });
+    //     this.lastModified = result.lastModified;
+
+    //     this.store.setItem(this.token + 'lastModified', this.lastModified);
+    //     this.store.setItem(this.token + 'apps_raw', this.appsMap);
+
+    //     return this.appsMap;
+    //   }),
+    //   shareReplay(),
+    // );
   }
 
   // 获取全部应用列表
   getAppList(): Observable<App[]> {
-    return this._getAppMapCache().pipe(map(m => compact(Array.from(m.values()))));
-  }
-
-  // 获取全部应用，以应用名为Key
-  getAppMap(): Observable<Map<string, App>> {
-    return this._getAppMapCache();
+    return this._getAppMapCache().pipe(map(m => compact(Object.values(m))));
   }
 
   // 根据应用名获取应用
   getAppByName(name: string): Observable<App> {
     return this._getAppMapCache().pipe(
-      map(m => m.get(name)),
+      map(m => m[name]),
       tap(app => console.log(`getAppByName:(${name}):`, app)),
     );
   }
 
   // 根据应用名列表获取应用列表
   getAppListByNames(appNames: string[]): Observable<App[]> {
-    return this._getAppMapCache().pipe(map(m => appNames.map(appName => m.get(appName))));
-  }
-
-  getAppMapByNames(appNames: string[]): Observable<Map<string, App>> {
-    return this._getAppMapCache().pipe(
-      map(m => {
-        const appMap = new Map<string, App>();
-        appNames
-          .filter(appName => m.has(appName))
-          .map(appName => appMap.set(appName, m.get(appName)));
-        return appMap;
-      }),
-    );
+    return this._getAppMapCache().pipe(map(m => appNames.map(appName => m[appName])));
   }
 
   // 设置Api网址
   setApiURL(apiURL: string) {
     this.apiURL = apiURL;
   }
+}
+
+interface AppMap {
+  [key: string]: App;
 }
 
 interface Result {
