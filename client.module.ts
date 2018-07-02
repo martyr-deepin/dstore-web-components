fed2c04cae6512685b0d0ca3de523a47b1fd21af
myml <wurongjie1@gmail.com>
2018-07-02 19:53:55 +0800

    fix: StoreService

--- client.module.ts
+++ client.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreService } from './services/store.service';
 
 @NgModule({
   imports: [CommonModule],
+  providers: [StoreService],
   exports: [],
   declarations: [],
 })
