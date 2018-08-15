8c4460541f8c1f664582b4c8a38c550d94414374
myml <wurongjie1@gmail.com>
2018-08-15 09:26:51 +0800

    share module

--- services/category.service.ts
+++ services/category.service.ts
@@ -1,31 +1,34 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { keyBy, first, toArray, throttle, groupBy } from 'lodash';
 import { Observable } from 'rxjs';
-import { retry, map, shareReplay } from 'rxjs/operators';
+import { map, shareReplay } from 'rxjs/operators';
 
-import { BaseService } from './base.service';
-import { Locale } from '../utils/locale';
-
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class CategoryService {
-  metadataServer = BaseService.serverHosts.metadataServer;
+  apiURL = environment.metadataServer + '/api/category';
+  categories$: Observable<{ [keys: string]: Category[] }>;
 
-  constructor(private http: HttpClient) {}
-
-  getList = throttle(this._getList, 60 * 1000);
-  private _getList(): Observable<{ [key: string]: Category }> {
-    return this.http.get<Category[]>(`${this.metadataServer}/api/category`).pipe(
-      retry(3),
-      map((categories: Category[]) => {
-        const localCategory = groupBy(categories, c => c.Locale);
-        return keyBy(
-          localCategory[Locale.getUnixLocale()] || first(toArray(localCategory)),
-          c => c.Name,
+  constructor(private http: HttpClient) {
+    this.categories$ = this.http.get(this.apiURL).pipe(
+      map((result: Category[]) => {
+        return result.reduce(
+          (acc, category) => {
+            return Object.assign(acc, {
+              [category.Locale]: [...(acc[category.Locale] || []), category],
+            });
+          },
+          {} as { [key: string]: Category[] },
         );
       }),
       shareReplay(),
     );
+  }
+
+  getList() {
+    return this.categories$;
   }
 }
 
